91b424d1f6736d6e5c837600488bae7d4fce39ec
Renier Trenuela <renier.trrenuela@gmail.com>
2024-10-23 22:37:43 +0800

    update server https

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Livewire\ProductsLivewire;
+use App\Models\WebHook;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', ProductsLivewire::class);
-Route::get('/verify-shopwire-webhook', function($request){
-    dd($request);
+Route::get('/verify-shopwire-webhook', function(Request $request){
+    WebHook::query()->fill(['result' => $request->input()]);
 });
